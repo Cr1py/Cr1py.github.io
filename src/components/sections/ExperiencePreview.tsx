2c080840e0b46aa2c7e26edd22c0428e08d35dd5
Christine Bautista <c.bautista2503@gmail.com>
2026-08-20 17:23:58 -0700

    Created sections for home page

---
+++ src/components/sections/ExperiencePreview.tsx
@@ -0,0 +1,14 @@
+
+const ExperiencePreview = () => {
+
+    return (
+        <div>
+            <h2>Experience</h2>
+            <p>
+                Experience will go here.
+            </p>
+        </div>
+    );
+};
+
+export default ExperiencePreview;
