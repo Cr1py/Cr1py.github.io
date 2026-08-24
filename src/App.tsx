0c8689fd7fb05cac7f800148f60ec15adf991663
Christine Bautista <c.bautista2503@gmail.com>
2026-08-24 05:29:16 -0700

    Added instant scrolling to top of page when navigating pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/layout/Layout";
+
 import Home from "./pages/Home";
 import Experience from "./pages/Experience";
 import Education from "./pages/Education";
@@ -7,18 +9,23 @@ import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetails";
 import Hobbies from "./pages/Hobbies";
 
+import ScrollToTop from "./components/interaction/ScrollToTop";
 
 const App = () => {
-    return (
+  return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Layout><Home /></Layout>} />
-          <Route path="/experience" element={<Layout><Experience /></Layout>} />
-          <Route path="/education" element={<Layout><Education /></Layout>} />
-          <Route path="/projects" element={<Layout><Projects /></Layout>} />
-          <Route path="/projects/:id" element={<Layout><ProjectDetail /></Layout>} />
-          <Route path="/hobbies" element={<Layout><Hobbies /></Layout>} />
-        </Routes>
+
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/experience" element={<Layout><Experience /></Layout>} />
+        <Route path="/education" element={<Layout><Education /></Layout>} />
+        <Route path="/projects" element={<Layout><Projects /></Layout>} />
+        <Route path="/projects/:id" element={<Layout><ProjectDetail /></Layout>} />
+        <Route path="/hobbies" element={<Layout><Hobbies /></Layout>} />
+      </Routes>
+
     </Router>
   );
 };
