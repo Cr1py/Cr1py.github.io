8481f1239ebbdc209766b4b44248b0551078e205
Christine Bautista <c.bautista2503@gmail.com>
2026-08-24 05:51:20 -0700

    added missing hobby pages and small nav bard fix

---
+++ src/pages/Baking.tsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+// consts
+const Baking = ()=> {
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-24 text-center">
+      <h4 className="text-2xl text-text mb-4">Page still in development. Come back later!</h4>
+    </div>
+  );
+};
+
+export default Baking;
